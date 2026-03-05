07014419beea960e912c2794a37ce9b7f49f6dbd
Goddy21 <goddy21@github.com>
2026-03-05 14:31:13 +0300

    ipv4

--- Backend/src/config/mailer.js
+++ Backend/src/config/mailer.js
@@ -1,9 +1,12 @@
 const nodemailer = require("nodemailer");
 
+dns.setDefaultResultOrder("ipv4first");
+
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT) || 587,
   secure: process.env.SMTP_SECURE === "true",
+  family: 4,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
